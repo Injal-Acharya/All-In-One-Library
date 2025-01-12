56d1323f237713c475b395f08dcc71ed6dcdcac7
AI-3Tech <injalacharya1@gmail.com>
2025-01-12 22:29:23 +0545

    Working on data fetching

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 let data = fetch("http://127.0.0.1:5500/modals/books.js")
 data.then((response) => {
     return response.json()
-
-})
-.then((value) => {
+}).then((value) => {
     console.log(value[0])
 })
