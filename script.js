9227171870f38b6d5d6a8fe127f2bf3e7a56ead0
AI-3Tech <injalacharya1@gmail.com>
2025-01-12 22:26:51 +0545

    data fetching initialized

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let data = fetch("http://127.0.0.1:5500/modals/books.js")
 data.then((response) => {
     return response.json()
-}).then((value) => {
+
+})
+.then((value) => {
     console.log(value[0])
 })
